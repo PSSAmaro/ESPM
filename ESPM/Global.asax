66d226ea95ccca3bf9b1f6a1be049de783682467
Paulo Sérgio Silva Amaro <2022312@student.uma.pt>
2017-05-31 20:08:53 +0100

    Add project files.

---
+++ ESPM/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="ESPM.MvcApplication" Language="C#" %>
---
+++ ESPM/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ESPM.Startup))]
+namespace ESPM
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
